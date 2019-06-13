da17563bdeb01c94f07101f4e13fbfbd4ba5c86f
Rosa Isela Lopez Palma <rosaiselalopezpalma@192.168.1.7>
2019-06-12 22:05:40 -0500

    Agregando la función encode y decode dentro del archivo cipher.js, dicha función códifica de manera correcta cualquier string, los ùnicos caràcteres que no se codifican son losespacios, las ñ y las comas, dichos carácteres se agregan tal cúal

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,34 @@
 window.cipher = {
-  // ... 
+
+  encode: (str, n) => {
+    let newStr = "";
+    const upperStr = str.toUpperCase();
+    console.log(upperStr)
+    for(let i = 0; i < upperStr.length; i++){ 
+      if(upperStr[i] === " " || upperStr[i] === "Ñ" || upperStr[i] === ","){
+        newStr += upperStr[i];
+      }else{
+        newPosition = ((upperStr.charCodeAt(i) - 65 + n) %  26) + 65;
+        newLetter = String.fromCharCode(newPosition);
+        newStr += newLetter;
+       }
+    }
+    return newStr;
+   },
+   
+
+  decode: (str, n) => {
+   let newStr = "";
+    const upperStr = str.toUpperCase();
+    for(let i = 0; i < upperStr.length; i++){ 
+      if(upperStr[i] === " " || upperStr[i] === "Ñ" || upperStr[i] === ","){
+        newStr += upperStr[i];
+      }else{
+        newPosition = (upperStr.charCodeAt(i) - n % 26);
+        newLetter = String.fromCharCode(newPosition);
+        newStr += newLetter;
+       }
+    }
+    return newStr;
+   },
 };
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
-document.getElementById("encriptar").addEventListener("click", function redirect(){
-  location.href="cifrar.html"
-})
+// document.getElementById("encriptar").addEventListener("click", function redirect(){
+//   location.href="cifrar.html"
+// })
 
-document.getElementById("desencriptar").addEventListener("click", function redirect(){
-  location.href="descifrar.html"
-})
+// document.getElementById("desencriptar").addEventListener("click", function redirect(){
+//   location.href="descifrar.html"
+// })
+
+console.log(window.cipher.decode("A B C", 2));
